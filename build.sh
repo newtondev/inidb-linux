3f52aa29ac17c06225c0f9e6a9d5124994064627
Craig Newton <craig.newton@teamtalkmedia.com>
2014-09-02 14:49:09 +0200

    Set build for 32 bit systems.

--- build.sh
+++ build.sh
@@ -5,10 +5,10 @@ rm -f bin/*
 for i in `ls src/*.cpp | sed -e 's/src\///g' | sed -e 's/\.cpp//g'`
 do
   echo $i
-  g++ -Wall -fPIC -g -c src/$i.cpp -o bin/$i.o
+  g++ -m32 -Wall -fPIC -g -c src/$i.cpp -o bin/$i.o
 done
 
 FILE_LIST=`ls bin/*.o`
-g++ -shared -Wl,-soname,inidb.so -o inidb.so $FILE_LIST
+g++ -m32 -shared -Wl,-soname,iniDB.so -o iniDB.so $FILE_LIST
 
 #g++ -Wall -shared -fPIC -m32 -o bin/libmyext.so src/libmyext.cpp
