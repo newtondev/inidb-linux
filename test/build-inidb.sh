e9acb1139e9812405a07f18dab7254641dc8f8ec
Craig Newton <craig.newton@teamtalkmedia.com>
2014-09-02 16:16:57 +0200

    Changed the case of inidb to iniDB.

--- test/build-inidb.sh
+++ test/build-inidb.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-g++ -m32 -fPIC -shared -o inidb.so ../src/main.cpp ../src/base64.cpp ../src/crc32.cpp ../src/ini.cpp ../src/logger.cpp ../src/md5.cpp ../src/privateprofile.cpp
+g++ -m32 -fPIC -shared -o iniDB.so ../src/main.cpp ../src/base64.cpp ../src/crc32.cpp ../src/ini.cpp ../src/logger.cpp ../src/md5.cpp ../src/privateprofile.cpp
--- test/main.cpp
+++ test/main.cpp
@@ -6,7 +6,7 @@ typedef void (*func_t)(char *output, int outputSize, const char *function);
 
 int main()
 {
-  void *shared_object = dlopen("./inidb.so", RTLD_LAZY);
+  void *shared_object = dlopen("./iniDB.so", RTLD_LAZY);
   func_t func = (func_t)dlsym(shared_object, "RVExtension");
   char *output = new char[1024];
 
